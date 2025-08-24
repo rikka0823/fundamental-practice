f506525c2e4ed3e9eb5d0bf89971eaa6041caaff
Rikka Chiu <shigoto312141@gmail.com>
2025-08-24 10:27:33 +0800

    Valid Parentheses, Merge Two Sorted Lists

--- src/leetCode/LeetCode.java
+++ src/leetCode/LeetCode.java
@@ -3,6 +3,7 @@ package leetCode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class LeetCode {
 
@@ -14,33 +15,185 @@ public class LeetCode {
         System.out.println(maxSubArray(new int[]{5, 4, -1, 7, 8}));
          */
 
+        /*
         // 1. Two Sum
         System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
         System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
         System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+         */
+
+        /*
+        // 20. Valid Parentheses
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([])"));
+        System.out.println(isValid("([)]"));
+         */
+
+        // 21. Merge Two Sorted Lists
+        System.out.println(mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))),
+                new ListNode(1, new ListNode(3, new ListNode(4)))));
     }
 
+    /**
+     * 21. Merge Two Sorted Lists
+     *
+     * You are given the heads of two sorted linked lists list1 and list2.
+     * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+     * Return the head of the merged linked list.
+     *
+     * Example 1:
+     * Input: list1 = [1,2,4], list2 = [1,3,4]
+     * Output: [1,1,2,3,4,4]
+     *
+     * Example 2:
+     * Input: list1 = [], list2 = []
+     * Output: []
+     *
+     * Example 3:
+     * Input: list1 = [], list2 = [0]
+     * Output: [0]
+     *
+     * Constraints:
+     * The number of nodes in both lists is in the range [0, 50].
+     * -100 <= Node.val <= 100
+     * Both list1 and list2 are sorted in non-decreasing order.
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    /**
+     * 20. Valid Parentheses
+     * <p>
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     * <p>
+     * An input string is valid if:
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     * <p>
+     * Example 1:
+     * Input: s = "()"
+     * Output: true
+     * <p>
+     * Example 2:
+     * Input: s = "()[]{}"
+     * Output: true
+     * <p>
+     * Example 3:
+     * Input: s = "(]"
+     * Output: false
+     * <p>
+     * Example 4:
+     * Input: s = "([])"
+     * Output: true
+     * <p>
+     * Example 5:
+     * <p>
+     * Input: s = "([)]"
+     * Output: false
+     * <p>
+     * Constraints:
+     * 1 <= s.length <= 104
+     * s consists of parentheses only '()[]{}'.
+     */
+    public static boolean isValid(String s) {
+        if (s == null || s.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 
     /**
      * 1. Two Sum
-     *
+     * <p>
      * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
      * You may assume that each input would have exactly one solution, and you may not use the same element twice.
      * You can return the answer in any order.
-     *
+     * <p>
      * Example 1:
      * Input: nums = [2,7,11,15], target = 9
      * Output: [0,1]
      * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-     *
+     * <p>
      * Example 2:
      * Input: nums = [3,2,4], target = 6
      * Output: [1,2]
-     *
+     * <p>
      * Example 3:
      * Input: nums = [3,3], target = 6
      * Output: [0,1]
-     *
+     * <p>
+     * Constraints:
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * Only one valid answer exists.
+     * <p>
+     * Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
      */
     public static int[] twoSum(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
