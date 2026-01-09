53e978c32f44984712d5c1c71534595a41b99c6b
Rikka Chiu <shigoto312141@gmail.com>
2026-01-09 20:30:46 +0800

    sql50 1. ~ 50.

---
+++ src/sql50/sql50.sql
@@ -0,0 +1,99 @@
+-- https://hackmd.io/@_7vFEnkKTve5g-aFhT8EvQ/Sy-H0QeWr
+
+-- 1.查詢學生表的前10條資料
+SELECT * FROM student LIMIT 10;
+
+-- 2.查詢成績表所有成績的最低分,平均分,總分
+SELECT MIN(sc.score) AS min_score, AVG(sc.score) AS avg_score, SUM(sc.score) as sum_score
+FROM score AS sc;
+
+-- 3.查詢老師 “諶燕” 所帶的課程設數量
+SELECT t.tname, COUNT(c.cno) AS courses
+FROM teacher AS t
+LEFT JOIN course AS c ON t.tno = c.tno
+WHERE t.tname = '諶燕'
+GROUP BY t.tname;
+
+
+-- 4.查詢所有老師所帶的課程數量
+SELECT t.tname, COUNT(c.cno) AS courses
+FROM teacher AS t
+LEFT JOIN course AS c ON t.tno = c.tno
+GROUP BY t.tname;
+
+-- 5.查詢姓”張”的學生名單
+SELECT * FROM student WHERE sname LIKE '張%';
+
+create database sql50;
+
+create table student(
+ sno varchar(10) primary key,
+ sname varchar(20),
+ sage int,
+ ssex varchar(5)
+);
+
+create table teacher(
+ tno varchar(10) primary key,
+ tname varchar(20)
+);
+
+create table course(
+ cno varchar(10),
+ cname varchar(20),
+ tno varchar(20),
+ constraint pk_course primary key (cno, tno)
+);
+
+create table score(
+ sno varchar(10),
+ cno varchar(10),
+ score decimal(4,2),
+ constraint pk_sc primary key (sno, cno)
+);
+
+insert into student values ('s001','張三',23,'男');
+insert into student values ('s002','李四',23,'男');
+insert into student values ('s003','吳鵬',25,'男');
+insert into student values ('s004','琴沁',20,'女');
+insert into student values ('s005','王麗',20,'女');
+insert into student values ('s006','李波',21,'男');
+insert into student values ('s007','劉玉',21,'男');
+insert into student values ('s008','蕭蓉',21,'女');
+insert into student values ('s009','陳蕭曉',23,'女');
+insert into student values ('s010','陳美',22,'女');
+insert into student values ('s011','王麗',24,'女');
+insert into student values ('s012','蕭蓉',20,'女');
+
+insert into teacher values ('t001', '劉陽');
+insert into teacher values ('t002', '諶燕');
+insert into teacher values ('t003', '胡明星');
+
+insert into course values ('c001','J2SE','t002');
+insert into course values ('c002','Java Web','t002');
+insert into course values ('c003','SSH','t001');
+insert into course values ('c004','Oracle','t001');
+insert into course values ('c005','SQL SERVER 2005','t003');
+insert into course values ('c006','C#','t003');
+insert into course values ('c007','JavaScript','t002');
+insert into course values ('c008','DIV+CSS','t001');
+insert into course values ('c009','PHP','t003');
+insert into course values ('c010','EJB3.0','t002');
+
+insert into score values ('s001','c001',78.9);
+insert into score values ('s002','c001',80.9);
+insert into score values ('s003','c001',81.9);
+insert into score values ('s004','c001',50.9);
+insert into score values ('s005','c001',59.9);
+insert into score values ('s001','c002',82.9);
+insert into score values ('s002','c002',72.9);
+insert into score values ('s003','c002',82.9);
+insert into score values ('s001','c003',59);
+insert into score values ('s006','c003',99.8);
+insert into score values ('s002','c004',52.9);
+insert into score values ('s003','c004',20.9);
+insert into score values ('s004','c004',59.8);
+insert into score values ('s005','c004',50.8);
+insert into score values ('s002','c005',92.9);
+insert into score values ('s001','c007',78.9);
+insert into score values ('s001','c010',78.9);
