5bda239def0eb25e0fc85fab62e9d7f501ef7908
Rikka Chiu <shigoto312141@gmail.com>
2026-01-18 15:33:26 +0800

    202. Happy Number

--- src/leetCode/LeetCode.java
+++ src/leetCode/LeetCode.java
@@ -131,10 +131,72 @@ public class LeetCode {
         System.out.println(Arrays.toString(intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})));
          */
 
+        /*
         // 387. First Unique Character in a String
         System.out.println(firstUniqChar("leetcode"));
         System.out.println(firstUniqChar("loveleetcode"));
         System.out.println(firstUniqChar("aabb"));
+         */
+
+        // 202. Happy Number
+        System.out.println(isHappy(19));
+        System.out.println(isHappy(2));
+
+    }
+
+    /**
+     * 202. Happy Number
+     *
+     * Write an algorithm to determine if a number n is happy.
+     *
+     * A happy number is a number defined by the following process:
+     * Starting with any positive integer, replace the number by the sum of the squares of its digits.
+     * Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+     * Those numbers for which this process ends in 1 are happy.
+     * Return true if n is a happy number, and false if not.
+     *
+     * Example 1:
+     * Input: n = 19
+     * Output: true
+     * Explanation:
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+     *
+     * Example 2:
+     * Input: n = 2
+     * Output: false
+     *
+     * Constraints:
+     * 1 <= n <= 231 - 1
+     */
+    public static boolean isHappy(int n) {
+        if (n < 1) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+            if (set.contains(n)) {
+                return false;
+            }
+            set.add(n);
+            n = getNextNum(n);
+        }
+
+        return true;
+    }
+
+    private static int getNextNum(int n) {
+        int sum = 0;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i++) {
+            sum += Integer.parseInt(String.valueOf( str.charAt(i)))
+                    * Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        return sum;
     }
 
     /**
